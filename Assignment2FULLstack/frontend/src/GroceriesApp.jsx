660bdd33337f50b88d59ad5a62c89f3707f562b5
SamuelDebruin <145690649+SamuelDebruin@users.noreply.github.com>
2023-12-04 21:49:17 -0500

    update
    s

--- Assignment2FULLstack/frontend/src/GroceriesApp.jsx
+++ Assignment2FULLstack/frontend/src/GroceriesApp.jsx
@@ -1,20 +1,58 @@
 import InventoryCard from "./InventoryCard";
 import CartList from "./CartList";
-import { products } from "./data/products";
-import { useState } from "react";
+//import { products } from "./data/products";
+import { useState, useEffect } from "react";
+import axios from "axios"
+import InventoryForm from "./InventoryForm.jsx";
 
 export default function GroceriesApp() {
-  //   const [counter, setCounter] = useState(1);
-  //   const addCounter = () => {
-  //     setCounter((prevCounter) => prevCounter + 1);
-  //   };
-
-  //   const subtractCounter = () => {
-  //     setCounter((prevCounter) => prevCounter - 1);
-  //   };
-
   const [cartList, setCartList] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [formData, setFormData] = useState({
+    id: "",
+    productName: "",
+    brand: "",
+    quantity: "",
+    image: "",
+    price: "",
+  });
+  const [responseData, setResponseData] = useState("");
 
+  //useEffect
+  useEffect(() => {
+    handleGetProducts();
+  }, [responseData]);
+
+  //HANDLERS
+  //Get products
+  const handleGetProducts = async() => {
+
+    await axios.get("http://localhost:3000/products").then((response) => {
+      setProducts(response.data);
+    });
+  };
+
+  //Post product
+  const handlePostProduct = async (product) => {
+    const postProduct = {
+      id: product.id,
+      productName: product.productName,
+      brand: product.brand,
+      quantity: product.quantity,
+      image: product.image,
+      price: product.price
+    }
+    await axios
+    .post("http://localhost:3000/addProduct", postProduct)
+    .then((response) => setResponseData(<p>{response.data}</p>));
+  }
+
+  const handleOnSubmit = (evt) => {
+    evt.perventDefualt();
+    handlePostProduct(formData)
+  }
+
+  //adding to cart
   const handleAddToCart = (item) => {
     setCartList((prevList) => {
       console.log(cartList);
@@ -22,10 +60,26 @@ export default function GroceriesApp() {
     });
   };
 
+
+  //onChange Handler
+  const handleOnChange = (evt) => {
+    const fieldName = evt.target.name;
+    const fieldValue = evt.target.value;
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        id: crypto.randomUUID(),
+        [fieldName]: fieldValue,
+      };
+    });
+  };
+
+  //Emptying cart
   const handleEmptyCart = () => {
     setCartList([]);
   };
 
+  //Remove Item from cart
   const handleRemoveItem = (id) => {
     setCartList((prevList) => {
       return prevList.filter((i) => i.id !== id);
@@ -35,6 +89,12 @@ export default function GroceriesApp() {
   return (
     <>
       <h1>Groceries App</h1>
+      <InventoryForm 
+      formData={formData} 
+      handleOnChange={handleOnChange} 
+      handleOnSubmit={handleOnSubmit}
+      />
+      {responseData}
       <div className="GroceriesApp-Container">
         <InventoryCard list={products} onClick={handleAddToCart} />
         <CartList
---
+++ Assignment2FULLstack/frontend/src/InventoryForm.jsx
@@ -0,0 +1,54 @@
+export default function InventoryForm(formData, handleOnChange, handleOnSubmit) {
+    return(
+    <div>
+        <form action="" onSubmit={() => handleOnSubmit(formData)}>
+            <div>
+                <label htmlFor="productName">product Name</label>
+                <input 
+                    type="text" 
+                    name="productName" 
+                    id="productName" 
+                    onChange={handleOnChange}
+                    value={formData.productName} />
+            </div>
+            <div>
+                <label htmlFor="brand">Brand</label>
+                <input 
+                    type="text" 
+                    name="brand" 
+                    id="brand" 
+                    onChange={handleOnChange}
+                    value={formData.brand} />
+            </div>
+            <div>
+                <label htmlFor="quantity">Qauntity</label>
+                <input 
+                    type="text" 
+                    name="quantity" 
+                    id="quantity" 
+                    onChange={handleOnChange}
+                    value={formData.quantity} />
+            </div>
+            <div>
+                <label htmlFor="image">Image URL</label>
+                <input 
+                    type="text" 
+                    name="image" 
+                    id="image" 
+                    onChange={handleOnChange}
+                    value={formData.image} />
+            </div>
+            <div>
+                <label htmlFor="price">Price</label>
+                <input 
+                    type="text" 
+                    name="price" 
+                    id="price" 
+                    onChange={handleOnChange}
+                    value={formData.price} />
+            </div>
+            <button>Add to Inventory</button>
+        </form>
+    </div>
+    );
+}
